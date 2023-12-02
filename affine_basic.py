c53eff0755c191ec2843f216302d6101b563cdf3
tester <1907284584@qq.com>
2023-12-02 21:16:40 +0800

    补充了Affine层的类

--- affine_basic.py
+++ affine_basic.py
@@ -1,8 +1,11 @@
-import numpy as np
+﻿import numpy as np
 
 # 输入长度
-line_in = 2
-line_out = 3
+
+Input = np.random.rand(5)  # 输入
+
+line_in = Input.shape[0]
+line_out = 4
 
 # 参数转移
 if 1 == 1:
@@ -10,21 +13,42 @@ if 1 == 1:
     b = line_out
 
 # 主函数
-X = np.random.rand(a)  # 输入
-W = np.random.rand(a, b)  # 权重
+X = Input  # 输入
+W = np.random.rand(b, a)  # 权重
 B = np.random.rand(b)  # 偏置
 
 
 class Affine:
-    def __init__(self):
+    def __init__(self, W, B):
         self.W = W
         self.B = B
-        self.X = X
+        self.X = None
         self.dW = None
         self.dB = None
 
+    def forward(self, X):
+        self.X = X
+        out = np.dot(self.W, X) + self.B
+        return out
 
-Y = np.dot(X, W) + B  # 输出
+    def backward(self, dout):
+        dX = np.dot(self.W.T, dout)
+        # self.dW = np.dot(self.X.T, dout)
+        self.dB = np.sum(dout, axis=0)
+        return dX
 
-print(X, W, B)
-print(Y)
+
+if __name__ == '__main__':
+    # Y = np.dot(X, W) + B  # 输出
+    afln = Affine(W, B)
+    print(np.around(X, 3))
+
+    X1 = afln.forward(X)
+
+    XZ = np.random.rand(b)
+    X2 = afln.backward(XZ)
+
+    print(np.around(X1, 3))  # 保留三位小数
+    print(np.around(XZ, 3))
+    print(np.around(X2, 3))
+    # print(X, W, B)
